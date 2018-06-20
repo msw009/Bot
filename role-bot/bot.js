3aa2e0a92daa933fad83877fa0bafb69131539f7
msw009 <36144660+msw009@users.noreply.github.com>
2018-06-20 00:28:48 -0500

    more setup

--- role-bot/bot.js
+++ role-bot/bot.js
@@ -3,38 +3,25 @@ const client = new Discord.Client();
 const config = require('./auth.json');
 
 client.on('ready', () => {
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-  
-  client.user.setActivity('Serving ${client.guilds.size} servers');
+	console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+
+	client.user.setActivity('Serving ${client.guilds.size} servers');
 });
 
 client.on('message', async message => {
-  if(message.author.bot) return;
-  if(message.content.indexOf(config.prefix) !== 0) return;
-  
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
+	if(message.author.bot) return;
+	if(message.content.indexOf(config.prefix) !== 0) return;
+
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+
+	if(command === "ping") {
+		const m = await message.channel.send("Ping?");
+		m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+	}
 	
-    if(command === "ping") {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-    const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  }
-  	
-  if (message.content === '!hi') {
-	message.reply('Hello!');
-  }
-  
-  
-  
+	
+
 });
 
-/*
-client.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.find('name', 'member-log');
-  if (!channel) return;
-  channel.send(`Welcome to the server, ${member}`);
-});
-*/
-
+client.login(config.token);
